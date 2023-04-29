0f6e44440e8996e3233af9b3b82b92e9b90b0521
zyk <venus.zyk@qq.com>
2023-04-29 23:35:49 +0800

    完成了RocketMQ整合，消息代理了提交答案

--- mq_service/src/main/java/com/kevin/service/MqProducerService.java
+++ mq_service/src/main/java/com/kevin/service/MqProducerService.java
@@ -3,57 +3,61 @@ package com.kevin.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kevin.pojo.BaseResponsePack;
 import com.kevin.pojo.TestUserResult;
-import org.apache.rocketmq.client.apis.ClientConfiguration;
-import org.apache.rocketmq.client.apis.ClientConfigurationBuilder;
-import org.apache.rocketmq.client.apis.ClientException;
-import org.apache.rocketmq.client.apis.ClientServiceProvider;
-import org.apache.rocketmq.client.apis.message.Message;
-import org.apache.rocketmq.client.apis.producer.Producer;
-import org.apache.rocketmq.client.apis.producer.SendReceipt;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MqProducerService {
-	public static final String TEST_TOPIC = "test";
-	public static final String SUBMIT_TAG = "submit";
+	private RocketMQTemplate rocketMqTemplate;
+	private ObjectMapper objectMapper = new ObjectMapper();
 
-	Producer testProducer;
-
-	ClientServiceProvider provider = ClientServiceProvider.loadService();
-	ObjectMapper objectMapper = new ObjectMapper();
-
-	public MqProducerService(@Value("${custom.broker-addr}") String endpoint) {
-		// 配置 provider 的目的地
-		ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint);
-		ClientConfiguration configuration = builder.build();
-		// 初始化Producer
-		try {
-			this.testProducer = provider.newProducerBuilder()
-					.setTopics(TEST_TOPIC)
-					.setClientConfiguration(configuration)
-					.build();
-		} catch (ClientException e) {
-			throw new RuntimeException(e);
-		}
+	@Autowired
+	public MqProducerService(RocketMQTemplate rocketMqTemplate) {
+		this.rocketMqTemplate = rocketMqTemplate;
 	}
 
+	/**
+	 * 同步的方式发送
+	 */
 	public BaseResponsePack sendTestRequest(TestUserResult testUserResult) {
 		try {
-			// 普通消息发送
-			Message message = provider.newMessageBuilder()
-					.setTopic(TEST_TOPIC)
-					// 设置消息Tag，用于消费端根据指定Tag过滤消息。
-					.setTag(SUBMIT_TAG)
-					// 消息体。
-					.setBody(objectMapper.writeValueAsString(testUserResult).getBytes())
-					.build();
-			// 发送消息，需要关注发送结果，并捕获失败等异常。
-			SendReceipt sendReceipt = testProducer.send(message);
-			System.out.println("发送成功");
-			return BaseResponsePack.simpleSuccess();
+			SendResult sendResult = rocketMqTemplate.syncSend("test:submit", objectMapper.writeValueAsString(testUserResult));
+			if (sendResult.getSendStatus() == SendStatus.SEND_OK) {
+				System.out.println("发送成功");
+				return BaseResponsePack.simpleSuccess();
+			} else {
+				System.out.println("发送失败");
+				return BaseResponsePack.simpleFail(String.valueOf(sendResult.getSendStatus()));
+			}
 		} catch (Exception e) {
-			return BaseResponsePack.simpleFail("Failed to send message: " + e.getMessage());
+			return BaseResponsePack.simpleFail("发送异常: " + e.getMessage());
 		}
 	}
+
+	/**
+	 * 	public static final String TEST_TOPIC = "test";
+	 * 	public static final String SUBMIT_TAG = "submit";
+	 *
+	 * 	Producer testProducer;
+	 *
+	 * 	ClientServiceProvider provider = ClientServiceProvider.loadService();
+	 * 	ObjectMapper objectMapper = new ObjectMapper();
+	 *
+	 * 	public MqProducerService(@Value("${custom.broker-addr}") String endpoint) {
+	 * 		// 配置 provider 的目的地
+	 * 		ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint);
+	 * 		ClientConfiguration configuration = builder.build();
+	 * 		// 初始化Producer
+	 * 		try {
+	 * 			this.testProducer = provider.newProducerBuilder()
+	 * 					.setTopics(TEST_TOPIC)
+	 * 					.setClientConfiguration(configuration)
+	 * 					.build();
+	 *                } catch (ClientException e) {
+	 * 			throw new RuntimeException(e);
+	 *        }* 	}
+	 */
 }
--- test_service/src/main/java/com/kevin/test_service/service/impl/TestSubmitService.java
+++ test_service/src/main/java/com/kevin/test_service/service/impl/TestSubmitService.java
@@ -3,61 +3,79 @@ package com.kevin.test_service.service.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kevin.test_service.pojo.TestUserResult;
 import com.kevin.test_service.service.TestUserResultService;
-import org.apache.rocketmq.client.apis.ClientConfiguration;
-import org.apache.rocketmq.client.apis.ClientServiceProvider;
-import org.apache.rocketmq.client.apis.consumer.ConsumeResult;
-import org.apache.rocketmq.client.apis.consumer.FilterExpression;
-import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
-import org.apache.rocketmq.client.apis.consumer.PushConsumer;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 
-@Component
-public class TestSubmitService {
+@Service
+@RocketMQMessageListener(consumerGroup = "testSubmitConsumerGroup", topic = "test", selectorExpression = "submit")
+public class TestSubmitService implements RocketMQListener<String> {
 	TestUserResultService testUserResultService;
-	public static final String TEST_TOPIC = "test";
-	public static final String SUBMIT_TAG = "submit";
 	public ObjectMapper objectMapper = new ObjectMapper();
 
-	PushConsumer pushConsumer;
-
 	@Autowired
-	public TestSubmitService(TestUserResultService testUserResultService, @Value("${custom.broker-addr}") String endpoints) {
-		System.out.println("TestSubmitService init");
+	public TestSubmitService(TestUserResultService testUserResultService) {
 		this.testUserResultService = testUserResultService;
-		// 配置Consumer
-		ClientServiceProvider provider = ClientServiceProvider.loadService();
-		ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
-				.setEndpoints(endpoints)
-				.build();
-		FilterExpression filterExpression = new FilterExpression(SUBMIT_TAG, FilterExpressionType.TAG);
-		// 初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
+	}
+
+	@Override
+	public void onMessage(String s) {
 		try {
-			this.pushConsumer = provider.newPushConsumerBuilder()
-					.setClientConfiguration(clientConfiguration)
-					// 设置预绑定的订阅关系。
-					.setSubscriptionExpressions(Collections.singletonMap(TEST_TOPIC, filterExpression))
-					.setConsumerGroup("testSubmitConsumerGroup")
-					// 设置消费监听器。
-					.setMessageListener(messageView -> {
-						// 处理消息并返回消费结果。
-						try {
-							TestUserResult result = objectMapper.readValue(messageView.getBody().array(), TestUserResult.class);
-							System.out.println("接收成功" + result);
-							//tochange 测试
-							testUserResultService.testSubmitAnswer(result);
-							return ConsumeResult.SUCCESS;
-						} catch (Exception e) {
-							return ConsumeResult.FAILURE;
-						}
-					})
-					.build();
+			TestUserResult result = objectMapper.readValue(s, TestUserResult.class);
+			//tochange 测试
+			testUserResultService.testSubmitAnswer(result);
 		} catch (Exception e) {
 			throw new RuntimeException(e.getMessage());
 		}
-
 	}
 }
+
+//
+//@Component
+//public class TestSubmitService {
+//	TestUserResultService testUserResultService;
+//	public static final String TEST_TOPIC = "test";
+//	public static final String SUBMIT_TAG = "submit";
+//	public ObjectMapper objectMapper = new ObjectMapper();
+//
+//	PushConsumer pushConsumer;
+//
+//	@Autowired
+//	public TestSubmitService(TestUserResultService testUserResultService, @Value("${custom.broker-addr}") String endpoints) {
+//		System.out.println("TestSubmitService init");
+//		this.testUserResultService = testUserResultService;
+//		// 配置Consumer
+//		ClientServiceProvider provider = ClientServiceProvider.loadService();
+//		ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+//				.setEndpoints(endpoints)
+//				.build();
+//		FilterExpression filterExpression = new FilterExpression(SUBMIT_TAG, FilterExpressionType.TAG);
+//		// 初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
+//		try {
+//			this.pushConsumer = provider.newPushConsumerBuilder()
+//					.setClientConfiguration(clientConfiguration)
+//					// 设置预绑定的订阅关系。
+//					.setSubscriptionExpressions(Collections.singletonMap(TEST_TOPIC, filterExpression))
+//					.setConsumerGroup("testSubmitConsumerGroup")
+//					// 设置消费监听器。
+//					.setMessageListener(messageView -> {
+//						// 处理消息并返回消费结果。
+//						try {
+//							TestUserResult result = objectMapper.readValue(messageView.getBody().array(), TestUserResult.class);
+//							System.out.println("接收成功" + result);
+//							//tochange 测试
+//							testUserResultService.testSubmitAnswer(result);
+//							return ConsumeResult.SUCCESS;
+//						} catch (Exception e) {
+//							return ConsumeResult.FAILURE;
+//						}
+//					})
+//					.build();
+//		} catch (Exception e) {
+//			throw new RuntimeException(e.getMessage());
+//		}
+//
+//	}
+//}
